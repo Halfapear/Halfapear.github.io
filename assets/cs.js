e2088183b217c0dab6abf5533f9ba9a06993a257
Halfapear <1783148122@qq.com>
2024-02-28 23:22:08 +0800

    200~​网安板块！代码雨和密码跳转（这次我没动什么脑)

--- assets/cs.js
+++ assets/cs.js
@@ -1,5 +1,6 @@
-const width = document.getElementById("myCanvas").width = screen.availWidth;
-const height = document.getElementById("myCanvas").height = screen.availHeight;
+// 代码雨逻辑
+const width = document.getElementById("myCanvas").width = window.innerWidth;
+const height = document.getElementById("myCanvas").height = window.innerHeight;
 const ctx = document.getElementById("myCanvas").getContext("2d");
 const arr = Array(Math.ceil(width / 10)).fill(0);
 const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
@@ -15,3 +16,18 @@ function rain() {
 }
 
 setInterval(rain, 30);
+
+// 表单提交逻辑
+document.getElementById('submit').addEventListener('click', function(event) {
+    event.preventDefault(); // 防止表单默认提交
+
+    // var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var correctPassword = "asd"; // 设置正确密码
+
+    if(password === correctPassword) {
+        window.location.href = "cs.html"; // 密码正确，跳转到正确的页面
+    } else {
+        window.location.href = "http://example.com/wrong-password-page"; // 密码错误，跳转到错误的页面
+    }
+});
