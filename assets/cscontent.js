ea1b13217839fb29705d74fdaa75968e0b9110a4
Halfapear <1783148122@qq.com>
2024-04-23 22:22:51 +0800

    4.23backup

---
+++ assets/cscontent.js
@@ -0,0 +1,7 @@
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('你好，世界！');
+});
+
+document.getElementById('skipButton').addEventListener('click', function() {
+    window.location.href = 'https://github.com/'; // 在这里修改为你想跳转的网址
+});
